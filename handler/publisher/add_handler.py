c3b5187cab7c2b1226ebcf62a8404849cf88ced6
wenyue <wenyue@360.cn>
2017-05-03 15:51:51 +0800

    add publisher

--- handler/publisher/add_handler.py
+++ handler/publisher/add_handler.py
@@ -5,13 +5,15 @@ import datetime
 
 from utils.exception import *
 from model import PublisherModel
-from handler.base.base_handler import BaseHandler
+from handler.base.base_handler import BaseHandler, handler
 
 
 class AddHandler(BaseHandler):
     """ 添加发布方 """
 
+    @handler
     def post(self):
+        current_time = datetime.datetime.now()
         name = self.get_json_argument('name')
         brief_introduction = self.get_json_argument('brief_introduction')
         logo = self.get_json_argument('logo')
@@ -19,15 +21,9 @@ class AddHandler(BaseHandler):
 
         publisher = self.model_config.first(PublisherModel, name=name)  # TODO: 发布方是否允许重名？
         if publisher:
-            raise ServerError(ServerError.USER_EXIST)
+            raise ServerError(ServerError.USER_EXIST, args=name)
 
-        publisher = PublisherModel()
-        publisher.name = name
-        publisher.brief_introduction = brief_introduction
-        publisher.logo = logo
-        publisher.address = address
-        publisher.is_del = '0'
-        publisher.create_time = datetime.datetime.now()
-        publisher.update_time = datetime.datetime.now()
+        publisher = PublisherModel(name=name, brief_introduction=brief_introduction, logo=logo, address=address,
+                                   is_del='0', create_time=current_time, update_time=current_time)
         self.model_config.add(publisher)
-        return ''
+        return {'publisher_id': publisher.id}
